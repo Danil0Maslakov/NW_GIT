4c385e5e7895c4378c9ace6fc07ea9ed88e431db
Danil0 <danil0maslakov@gmail.com>
2023-12-10 16:42:50 +0300

    Networking app using ASIO library

---
+++ Nw/FileName.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<asio.hpp>
+#include<vector>
+using  asio::ip::tcp;
+int main(int argc, char* argv[]) {
+	try {
+		if (argc != 2) {
+			std::cerr << "Usage client host" << std::endl;
+			return 1;
+		}
+
+		asio::io_context io_context;
+		tcp::resolver resolver(io_context);
+		tcp::resolver::results_type endpoints = resolver.resolve(argv[1], "daytime");
+		tcp::socket socket(io_context);
+		asio::connect(socket, endpoints);
+
+		for (;;) {
+			std::vector<char>buf(128);
+			asio::error_code error;
+			size_t len = socket.read_some(asio::buffer(buf), error);
+
+			if (error = asio::error::eof)
+			{
+				break;
+			}
+			else
+			{
+				throw(error);
+			}
+		}
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what()<<std::endl;
+	}
+}
